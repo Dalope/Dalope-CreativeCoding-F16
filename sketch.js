2f34aec27fb8fc327aa63b0045dee2bd3fe220ba
Dalope <gd900@nyu.edu>
2016-11-30 23:28:24 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,28 +1,74 @@
-var rectWidth = 100
-var rectHeight = 50
+var canvas;
+var url;
+var globalData;
 
-var rStep = 50
-var gStep = 200
-var bStep = 70
+var urlBase;
+var apiKey;
+var query;
+
+// var index = 0;
+
+var searchBox;
+var searchButton;
 
 function setup() {
-	createCanvas(1000, 1000);
+	canvas = createCanvas(500, 500);
+	canvas.position(300, 150);
+
+	urlBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+	apiKey = "eb8367adf06a4fb6b301e02157a03a73";
+	// query = myTerms[index];
+
+	// url = urlBase + "?api-key=" + apiKey + "&q=" + searchRes;
+
+	// setInterval(loadNewData, 2000);
+
+	searchBox = createInput('Search the NY Times');
+	searchBox.position(300, 100);
+
+	searchButton = createButton("Search");
+	searchButton.position(450, 100);
+
+	searchButton.mousePressed(display);
+
+
+
 }
 
-function draw() {
-	noStroke();
 
-	for (i = 1; i<=4; i++){
-		push();
-
-		fill(255-i*rStep, 200-i*gStep, 176-i*bStep);
-		translate((i*(width/5) - (rectWidth/2)), (height/2) - (rectHeight/2));
-
-		rotate((2*i-1)*PI/4);
-
-		rect(0, 0, rectWidth, rectHeight);
-
-		pop();
-
+//callback function
+function gotData(myData) {
+	globalData = myData;
+	for (i = 0; i<=5; i++){
+		console.log(globalData.response.docs[i].headline.main);
 	}
+
+}
+
+//repeating interval function
+function loadNewData() {
+	myData = loadJSON(url, gotData);
+	// index++;
+	// if(index >= 3){
+	// 	index = 0;
+	// }
+
+}
+
+
+function draw() {
+	background(255);
+	if(globalData){
+		for (i = 0; i<=5; i++){
+			text(globalData.response.docs[i].headline.main, 0, 50*i, 500, 500);
+		}
+	}
+}
+
+
+function display() {
+	var searchRes = searchBox.value();
+
+	url = urlBase + "?api-key=" + apiKey + "&q=" + searchRes;
+	loadNewData();
 }
